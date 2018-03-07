756f9f998ca347e160e47101e2e73c263722fc32
MatthewMelvinBelcher <31696563+MatthewMelvinBelcher@users.noreply.github.com>
2018-03-07 12:29:37 +0000

    Working code
    Working yt plugin code... tracks video and updates sql.

--- plugins/yt/xchange.php
+++ plugins/yt/xchange.php
@@ -1,7 +1,10 @@
 <div class="contentbox">
     <div class="head">Youtube</div>
     <div class="contentinside">
+
         <?php
+
+
         foreach($_GET as $key => $value) {
         $gets[$key] = filter($value);
         }
@@ -9,7 +12,7 @@
             $sit1 = mysql_query("SELECT * FROM `youtube` WHERE `id`='{$gets['id']}'");
             $sit = mysql_num_rows($sit1);
             if($sit > 0){
-            mysql_query("INSERT INTO `watched` (user_id, site_id) VALUES('{$data->id}','{$gets['id']}')");
+            mysql_query("INSERT INTO `watched` ('user_id', 'site_id') VALUES('{$data->id}','{$gets['id']}')");
         }}}
 
         $mysite = mysql_query("SELECT * FROM `youtube` WHERE `active` = '0' AND (SELECT `coins` FROM `users` WHERE `id` = `youtube`.`user` ) >= `cpc` AND `id` NOT IN (SELECT `site_id` FROM `watched` WHERE `user_id`='{$data->id}') ORDER BY `cpc` DESC LIMIT 0, 1");
@@ -18,88 +21,109 @@
         if($ext > 0){
             ?>
             <script src="swfobject.js"></script>
+
         <script type="text/javascript">
-        var playing = false;
-        var fullyPlayed = false;
-        var interval = '';
-        var played = 0;
-        var length = 10;
-        
-        function YouTubePlaying(){
-            played += 0.1;
-            roundedPlayed = Math.ceil(played);
-            document.getElementById("played").innerHTML = Math.min(roundedPlayed,length);
-            if (roundedPlayed == length){
-                if (fullyPlayed == false){
-                    YouTubePlayed();
-                    fullyPlayed = true;
-                }
-            }
-        }
-        function YouTubePlayed(){
+        </script>
+        <?php
+        $url = explode('watch?v=', $site2->url);
+        $url = $url[1];
+
+        ?>
+        <div id="Hint" class="hint"></div>
+        <center>
+        View this video for 10 seconds and after that you will receive <? echo $site2->cpc - 1;?> coins<br/><br/>
+<div id="player"></div>
+<script src="https://www.youtube.com/player_api"></script>
+<script>
+    /**
+     * Put your video IDs in this array
+     */
+    var videoIDs = [
+        '<? echo $url;?>',// PHP Code Display
+        '<? echo $url;?>', // PHP Code Display
+
+    ];
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = false;
+    ga.src = 'http://www.youtube.com/player_api';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+    var player, currentVideoId = 0;
+    var timestamp = 10;
+    var timer;
+    var done = false;
+    var response = '<? echo $site2->id;?>';
+    var cpc = '<? echo $site2->cpc - 1;?>';
+    var userid = '<? echo $data->id;?>';
+    
+                       
+
+ 
+    function timestamp_reached() {
+       alert('you have gained ' + cpc + ' coins!');
             document.getElementById("Hint").style.display='block';
             $("#Hint").html('Please Wait...');
-            var response = '<? echo $site2->id;?>';
-            var cpc = '<? echo $site2->cpc - 1;?>';
-            var userid = "<? echo $data->id;?>";
             $.ajax({
-                type: "POST",
+                type: 'POST',
+                                type: "POST",
                 url: "plugins/yt/complete.php",
                 data: "site=" + response +"&userid=" + userid,
+
                 success: function(msg){
-                    var coins = $("#coins").html().replace(" Coins", "");
+var coins = $("#coins").html().replace(" Coins", "");
                     coins = parseInt(coins) + parseInt(cpc);
                     $("#coins").html(coins + " Coins");
                     $("#Hint").html('You have gained ' + cpc + ' coins!');
                 }
             });
-            document.getElementById(response).style.visibility = "visible";
+    }
+
+    function timestamp_callback() {
+        clearTimeout(timer);
+            
+        current_time = player.getCurrentTime();
+            //alert(current_time);
+        remaining_time = timestamp - current_time;
+            //alert(remaining_time);
+        if (remaining_time > 0) {
+            timer = setTimeout(timestamp_reached, remaining_time * 1000);
+        }    
+    }
+    
+    function onPlayerStateChange(evt) {
+            //alert(evt.data);
+        if (evt.data == YT.PlayerState.PLAYING) {
+            timestamp_callback();
         }
-        
-        function onYouTubePlayerReady(playerId){
-            ytplayer = document.getElementById("myytplayer");
-            ytplayer.addEventListener("onStateChange", "onYouTubePlayerStateChange");
-        }
-        function onYouTubePlayerStateChange(newState){
-            if (newState == 1){
-                playing = true;
-                interval = window.setInterval('YouTubePlaying()',100);
-            }else if (newState == 0){
-                if (playing) window.clearInterval(interval);
-                playing = false;
-                ytplayer.stopVideo();
-                ytplayer.playVideo();
-            }else{
-                if (playing) window.clearInterval(interval);
-                playing = false;
+    };
+ 
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+            height: '350',
+            width: '425',
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
             }
-        }
-        function refreshpage()
-        {
-            window.location.reload();
-        }
-        </script>
-        <?php
-        $url = explode('watch?v=', $site2->url);
-        $url = $url[1];
-        ?>
-        <div id="Hint" class="hint"></div>
-        <center>
-        View this video for 10 seconds and after that you will receive <? echo $site2->cpc - 1;?> coins<br/><br/>
-        <div id="ytPlayer">You need Flash player 8+ and JavaScript enabled to view this video.</div>
-        <script type="text/javascript">
-        var params = { allowScriptAccess: "always" };
-        var atts = { id: "myytplayer" };
-        swfobject.embedSWF("http://www.youtube.com/v/<? echo $url;?>?enablejsapi=1&playerapiid=ytplayer", "ytPlayer", "425", "356", "8", null, null, params, atts);
-        </script>
+        });
+    }
+
+    function onPlayerReady(event) {
+        event.target.loadVideoById(videoIDs[currentVideoId]);
+    }
+
+
+</script>
+
         <br/>
         
-        <br />Must play for <span id="played">0</span>/10 seconds (<a href="xchange.php?p=yt&a=skip&id=<? echo $site2->id;?>" style="color:blue">Skip</a>)
+        <br /><div>Must play for <span id="played" style="color:blue"></span>10 seconds (<a href="xchange.php?p=yt&id=<? echo $site2->id;?>" style="color:blue">Skip</a>)</div>
         <div id="<? echo $site2->id;?>" style="visibility:hidden"><a href="javascript:refreshpage()">View Next Video</a></div>
         </center>
     
     
         <?php
-        }else{ echo '<div class="error">ERROR: Tut uns leid: Es sind keine Coins mehr vorhanden! Bitte versuchen sie es in ein paar Minuten erneut!</div>'; } ?>
+        }else{ echo '<div class="error">ERROR: Sorry, there are no coins left! Please try again in a few minutes!</div>'; } ?>
     </div>
 </div>
